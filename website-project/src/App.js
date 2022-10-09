e504a613eb655872c4c7e1020a0e49d92b18320f
qlaive <tdsfee@gmail.com>
2022-10-09 12:10:00 -0500

    Changes

--- website-project/src/App.js
+++ website-project/src/App.js
@@ -29,7 +29,7 @@ function App() {
           </Introduction>
           <hr/>
           <Download>
-            <ResumeHeader>My work History <BriefCase className='ml-4 h-8 w-8 stroke-2'/></ResumeHeader>
+            <ResumeHeader>My work History <BriefCase className='ml-4 h-8 w-8 stroke-2 text-gray-300'/></ResumeHeader>
             <Row>
               <WorkImage src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.acellus.com%2Fwp-content%2Fuploads%2F2017%2F09%2FAcellus-Cube-Logo-Color.jpg&f=1&nofb=1&ipt=f95bd14bf42128cd18f77a63f97797c1bb038d9ddc2330a621dc69b3691b9466&ipo=images" />
               <Description>
@@ -83,25 +83,25 @@ export default App;
 const Container = tw.div`bg-black h-[100%] w-[100%] min-h-[110vh]`;
 const NextContainer = tw.div`bg-gray-600 bg-opacity-20 h-[100%] w-full md:w-3/4 min-h-[100vh] mx-0 md:mx-auto p-2 md:p-4 pb-16`;
 
-const Header = tw.h1`text-5xl text-center text-white font-bold pt-32 w-full lg:w-1/2 pb-9 mx-auto md:mx-0 pr-0 md:pr-8`;
+const Header = tw.h1`text-blue-500 text-5xl text-center font-bold pt-32 w-full xl:w-1/2 pb-9 mx-auto md:mx-0 pr-0 md:pr-8`;
 const ImageContainer = tw.div`flex-col`;
 const Caption = tw.p` text-gray-500 opacity-10 hover:opacity-100 hover:text-white transition ease-in-out duration-300 font-bold text-sm w-full mx-auto md:ml-8 mt-4`;
 const Image = tw.img`rounded-full h-20 w-20 ml-52 border-white border-2 ring-black ring-2`;
 
 const Download = tw.div`mt-8 rounded-2xl text-lg font-bold border-gray-500 border mx-auto w-full md:w-5/6 whitespace-nowrap`;
-const ResumeHeader = tw.div`flex flex-row text-lg font-bold text-white mt-8 mb-4 justify-center`;
+const ResumeHeader = tw.div`flex flex-row text-lg font-bold text-red-500 mt-8 mb-4 justify-center`;
 const Row = tw.div`flex flex-row mt-4 mx-4 mr-16 justify-start`;
 const Description = tw.div`flex flex-col ml-8 mr-auto w-1/2`;
-const Company = tw.h2`text-base font-semibold`;
+const Company = tw.h2`text-green-500 text-base font-semibold`;
 const CompanyTitle = tw.p`text-sm text-gray-500`;
 const Date = tw.p`text-gray-600 text-xs`;
 const WorkImage = tw.img`mr-2 rounded-full h-10 w-10`;
-const Resume = tw.div`flex flex-col text-white font-bold text-sm text-center mt-16 align-baseline`;
-const ResumeButton = tw.button`flex justify-center bg-gray-800 bg-opacity-50 font-bold text-sm mx-auto my-4 py-4 align-baseline border border-gray-500 rounded-2xl h-3/5 w-10/12 hover:bg-white hover:text-black hover:border-black transition ease-in-out duration-300`;
+const Resume = tw.div`text-yellow-300 flex flex-col font-bold text-sm text-center mt-16 align-baseline`;
+const ResumeButton = tw.button`text-white flex justify-center bg-gray-800 bg-opacity-50 font-bold text-sm mx-auto my-4 py-4 align-baseline border border-gray-500 rounded-2xl h-3/5 w-10/12 hover:bg-white hover:text-black hover:border-black transition ease-in-out duration-300`;
 const DocSVG = tw(DocDownload)`h-6 w-6 ml-2 text-white stroke-2 hover:text-black`;
 
 const Content = tw.div` block  lg:flex w-full h-full text-gray-300 mx-auto text-xl pl-4 pr-4 pb-4 my-8 mb-8 justify-between`;
-const Introduction = tw.p`text-base text-white w-full h-full mb-8 mr-8`;
+const Introduction = tw.p`text-base text-gray-300 w-full h-full mb-8 mr-8`;
 
 const SubHeaderContainer = tw.div`flex justify-center items-center`;
 const Subheader = tw.h2`text-6xl font-bold text-gray-200 animate-pulse pb-16`;
